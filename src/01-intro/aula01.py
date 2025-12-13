1101ef3ae72528665a10c4da2725dc8fdae1532c
Vitor Medeiros <vitorsimedeiros@gmail.com>
2025-12-13 15:07:42 -0300

    first commit

---
+++ src/01-intro/aula01.py
@@ -0,0 +1,11 @@
+""" aula 01 """
+print("olá mundo!!!")
+
+
+def somar(numero_01, numero_02):
+    """ somar - soma dois números """
+    return numero_01+numero_02
+
+
+SOMA = somar(10.3, 2.5)
+print(SOMA)
