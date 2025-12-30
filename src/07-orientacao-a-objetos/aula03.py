7aa5bacf3d81d6edc598502519a74df344d711e5
Vitor Medeiros <vitorsimedeiros@gmail.com>
2025-12-30 17:45:24 -0300

    add aula03.py implementing class methods for the Rentangulo class

---
+++ src/07-orientacao-a-objetos/aula03.py
@@ -0,0 +1,30 @@
+""" Aula 03 - Métodos de classe """
+
+class Rentangulo:
+    def __init__(self, base, altura):
+        self.base = base
+        self.altura = altura
+
+    @classmethod
+    def from_list(cls, lista):
+        return cls(lista[0], lista[1])
+
+    @classmethod
+    def from_string(cls, rep_retungulo):
+        base, altura = rep_retungulo.split(sep=',')
+        return cls(float(base), float(altura))
+
+    def calcular_area(self):
+        return self.base * self.altura
+
+    def calcular_perimetro(self):
+        return 2 * (self.base + self.altura)
+
+
+retangulo1 = Rentangulo(10.0, 5.0)
+retangulo2 = Rentangulo(6.0, 3.0)
+retangulo3 = Rentangulo.from_list([20.0, 3.5])
+retangulo4 = Rentangulo.from_string("55.4,13.5")
+
+print(retangulo3.base, retangulo3.altura, retangulo3.calcular_area())
+print(retangulo4.base, retangulo4.altura, retangulo4.calcular_area())
